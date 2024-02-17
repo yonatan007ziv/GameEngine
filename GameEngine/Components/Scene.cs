4bc3d7a6b0e86897ad09731d5a34a53a3d803125
yonatan007ziv <yonatan007ziv@gmail.com>
2024-02-18 01:07:30 +0200

    Added option to remove objects from scene

--- GameEngine/Components/Scene.cs
+++ GameEngine/Components/Scene.cs
@@ -2,27 +2,66 @@
 using GameEngine.Components.Objects;
 using GameEngine.Core.Components;
 using GameEngine.Core.Components.Input.Buttons;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 
 namespace GameEngine.Components;
 
 public class Scene : IInputMapper, IDisposable
 {
-	private static Scene? _loadedScene;
+	public static Scene LoadedScene { get; private set; } = null!;
 
 	private bool _loaded;
 
-	protected List<(WorldComponent camera, ViewPort viewPort)> worldCameras = new List<(WorldComponent camera, ViewPort viewPort)>();
-	protected List<WorldObject> worldObjects = new List<WorldObject>();
+	public readonly ObservableCollection<(WorldComponent camera, ViewPort viewPort)> worldCameras = new ObservableCollection<(WorldComponent camera, ViewPort viewPort)>();
+	public readonly ObservableCollection<WorldObject> worldObjects = new ObservableCollection<WorldObject>();
 
-	protected List<(UIComponent camera, ViewPort viewPort)> uiCameras = new List<(UIComponent camera, ViewPort viewPort)>();
-	protected List<UIObject> uiObjects = new List<UIObject>();
+	public readonly ObservableCollection<(UIComponent camera, ViewPort viewPort)> uiCameras = new ObservableCollection<(UIComponent camera, ViewPort viewPort)>();
+	public readonly ObservableCollection<UIObject> uiObjects = new ObservableCollection<UIObject>();
+
+    public Scene()
+    {
+		worldCameras.CollectionChanged += WorldCamerasChanged;
+		worldObjects.CollectionChanged += WorldObjectsChanged;
+		uiCameras.CollectionChanged += UICamerasChanged;
+		uiObjects.CollectionChanged += UIObjectsChanged;
+	}
+
+	private void WorldCamerasChanged(object? sender, NotifyCollectionChangedEventArgs e)
+	{
+		if (_loaded && e.Action == NotifyCollectionChangedAction.Remove)
+			foreach (object obj in e.OldItems!)
+				if (obj is WorldComponent worldCamera)
+					Services.Implementations.GameEngine.EngineContext.RemoveWorldCamera(worldCamera);
+	}
+	private void WorldObjectsChanged(object? sender, NotifyCollectionChangedEventArgs e)
+	{
+		if (_loaded && e.Action == NotifyCollectionChangedAction.Remove)
+			foreach (object obj in e.OldItems!)
+				if (obj is WorldObject worldObject)
+					Services.Implementations.GameEngine.EngineContext.RemoveWorldObject(worldObject);
+	}
+	private void UICamerasChanged(object? sender, NotifyCollectionChangedEventArgs e)
+	{
+		if (_loaded && e.Action == NotifyCollectionChangedAction.Remove)
+			foreach (object obj in e.OldItems!)
+				if (obj is UIComponent uiCamera)
+					Services.Implementations.GameEngine.EngineContext.RemoveUICamera(uiCamera);
+	}
+	private void UIObjectsChanged(object? sender, NotifyCollectionChangedEventArgs e)
+	{
+		if (_loaded && e.Action == NotifyCollectionChangedAction.Remove)
+			foreach (object obj in e.OldItems!)
+				if (obj is UIObject uiObject)
+					Services.Implementations.GameEngine.EngineContext.RemoveUIObject(uiObject);
+	}
 
 	public async void LoadScene()
 	{
 		await Services.Implementations.GameEngine.EngineContext.EngineLoadingTask.Task;
 
-		if (_loadedScene is not null)
-			_loadedScene.UnloadScene();
+		if (LoadedScene is not null)
+			LoadedScene.UnloadScene();
 
 		foreach (WorldObject worldObject in worldObjects)
 			Services.Implementations.GameEngine.EngineContext.AddWorldObject(worldObject);
@@ -35,7 +74,7 @@ public class Scene : IInputMapper, IDisposable
 			Services.Implementations.GameEngine.EngineContext.AddUICamera(camera, viewPort);
 
 		_loaded = true;
-		_loadedScene = this;
+		LoadedScene = this;
 	}
 
 	public void UnloadScene()
@@ -46,7 +85,7 @@ public class Scene : IInputMapper, IDisposable
 			Services.Implementations.GameEngine.EngineContext.RemoveWorldCamera(camera);
 
 		_loaded = false;
-		_loadedScene = null;
+		LoadedScene = null!;
 	}
 
 	public void Dispose()
--- Sample1/GameObjects/Trex.cs
+++ Sample1/GameObjects/Trex.cs
@@ -1,12 +1,19 @@
-﻿using GameEngine.Components.Objects;
+﻿using GameEngine.Components;
+using GameEngine.Components.Objects.Scriptable;
 using GameEngine.Core.Components;
 
 namespace Sample1.GameObjects;
 
-internal class Trex : WorldObject
+internal class Trex : ScriptableWorldObject
 {
 	public Trex()
 	{
 		Meshes.Add(new MeshData("Trex.obj", "Hamama.mat"));
+	}
+
+	public override void Update(float deltaTime)
+	{
+		if (GetKeyboardButtonDown(GameEngine.Core.Components.Input.Buttons.KeyboardButton.Q))
+			Scene.LoadedScene.worldObjects.Remove(this);
 	}
 }
